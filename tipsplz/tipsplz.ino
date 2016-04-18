c08f0db0e1b4b982a042a10d7846132011a6c8a0
creativethumbs <ticha.seth@gmail.com>
2016-04-18 13:48:50 -0400

    everything is messed up, saving progress

--- tipsplz/tipsplz.ino
+++ tipsplz/tipsplz.ino
@@ -1,5 +1,5 @@
 // !! Requires Arduino-MOS scheduler library
-#include <MOS.h>
+//#include <MOS.h>
 #include <Wire.h> // Enable this line if using Arduino Uno, Mega, etc.
 #include <elapsedMillis.h>
 
@@ -16,14 +16,14 @@
 #define KA1     11
 #define KAS1    12
 #define KB1     13
-#define KC2     14
-#define KCS2    15
-#define KD2     16
-#define KDS2    17
-#define KE2     18
-#define KF2     19
-#define KFS2    20
-#define KG2     21
+#define KC2     32
+#define KCS2    34
+#define KD2     36
+#define KDS2    38
+#define KE2     40
+#define KF2     42
+#define KFS2    44
+#define KG2     46
 #define KGS2    22
 #define KA2     24
 #define KAS2    26
@@ -31,14 +31,14 @@
 #define KC3     30
 
 // chords
-#define E1_A1_C2_E2    31
-#define F1_A1_C2_DS2   32
-#define F1_A1_D2       33
-#define D1_F1_C2       34
-#define D1_F1_A1       35
-#define D1_FS1_C2      36
-#define C1_E1_A1       37
-#define A1_C2_E2_A2    38
+#define E1_A1_C2_E2    51
+#define F1_A1_C2_DS2   52
+#define F1_A1_D2       53
+#define D1_F1_C2       54
+#define D1_F1_A1       55
+#define D1_GS1_C2      56
+#define C1_E1_A1       57
+#define A1_C2_E2_A2    58
 
 #define REST    -1
 
@@ -47,7 +47,7 @@ const int sampleWindow = 50; // Sample window width in mS (50 mS = 20Hz)
 unsigned int sample;
 
 // time stamp for the last time a tip was received
-unsigned long lastTip;
+unsigned long lastTip = -5000;
 
 int peaks = 0;
 
@@ -59,70 +59,140 @@ int LoopPosition = 0;
 int LoopNote;
 int LoopDuration;
 
+boolean playingNote = false;
+int noteStartTime; 
+
+int Loops2[45][2] = { {E1_A1_C2_E2, 150}, {REST, 100}, {E1_A1_C2_E2, 150}, {REST, 100}, {E1_A1_C2_E2, 150}, {REST, 100}, {E1_A1_C2_E2, 150}, {REST, 100}, {E1_A1_C2_E2, 150}, {REST, 100}, {E1_A1_C2_E2, 150}, {REST, 100},
+  {KE1, 125}, {KA1, 125}, {KC2, 125}, {KE2, 125},
+  {F1_A1_C2_DS2, 150}, {REST, 100}, {F1_A1_C2_DS2, 150}, {REST, 100}, {F1_A1_C2_DS2, 150}, {REST, 100}, {F1_A1_C2_DS2, 150}, {REST, 100}, {F1_A1_C2_DS2, 150}, {REST, 100}, {F1_A1_C2_DS2, 150}, {REST, 100},
+  {KF1, 125}, {KA1, 125}, {KC2, 125}, {KDS2, 125},
+  {F1_A1_D2, 400}, {REST, 100},
+  {D1_F1_C2, 150}, {REST, 100}, {D1_F1_A1, 150}, {REST, 100}, {D1_GS1_C2, 150}, {REST, 100}, {D1_GS1_C2, 500}, {C1_E1_A1, 1250},
+  {A1_C2_E2_A2, 250}, {REST, 250}, {REST, 500}
+};
+
 int Loops [6][45][2] = {
   // initial loop (no money :( )
   {{E1_A1_C2_E2, 250}, {REST, 1750}},
 
   // second loop (tiny bit of money)
-  {{E1_A1_C2_E2, 200}, {REST, 50}},
+  {{E1_A1_C2_E2, 150}, {REST, 100}},
 
   // third loop (little bit more money)
-  { {E1_A1_C2_E2, 200}, {REST, 50}, {E1_A1_C2_E2, 200}, {REST, 50}, {E1_A1_C2_E2, 200}, {REST, 50}, {E1_A1_C2_E2, 200}, {REST, 50}, {E1_A1_C2_E2, 200}, {REST, 50}, {E1_A1_C2_E2, 200}, {REST, 50},
+  { {E1_A1_C2_E2, 150}, {REST, 100}, {E1_A1_C2_E2, 150}, {REST, 100}, {E1_A1_C2_E2, 150}, {REST, 100}, {E1_A1_C2_E2, 150}, {REST, 100}, {E1_A1_C2_E2, 150}, {REST, 100}, {E1_A1_C2_E2, 150}, {REST, 100},
     {KE1, 125}, {KA1, 125}, {KC2, 125}, {KE2, 125}
   },
 
   // fourth loop (little bit more money)
-  { {E1_A1_C2_E2, 200}, {REST, 50}, {E1_A1_C2_E2, 200}, {REST, 50}, {E1_A1_C2_E2, 200}, {REST, 50}, {E1_A1_C2_E2, 200}, {REST, 50}, {E1_A1_C2_E2, 200}, {REST, 50}, {E1_A1_C2_E2, 200}, {REST, 50},
+  { {E1_A1_C2_E2, 150}, {REST, 100}, {E1_A1_C2_E2, 150}, {REST, 100}, {E1_A1_C2_E2, 150}, {REST, 100}, {E1_A1_C2_E2, 150}, {REST, 100}, {E1_A1_C2_E2, 150}, {REST, 100}, {E1_A1_C2_E2, 150}, {REST, 100},
     {KE1, 125}, {KA1, 125}, {KC2, 125}, {KE2, 125},
-    {F1_A1_C2_DS2, 200}, {REST, 50}, {F1_A1_C2_DS2, 200}, {REST, 50}, {F1_A1_C2_DS2, 200}, {REST, 50}, {F1_A1_C2_DS2, 200}, {REST, 50}, {F1_A1_C2_DS2, 200}, {REST, 750}
+    {F1_A1_C2_DS2, 150}, {REST, 100}, {F1_A1_C2_DS2, 150}, {REST, 100}, {F1_A1_C2_DS2, 150}, {REST, 100}, {F1_A1_C2_DS2, 150}, {REST, 100}, {F1_A1_C2_DS2, 150}, {REST, 750}
   },
 
   // fifth loop (little bit more money)
-  { {E1_A1_C2_E2, 200}, {REST, 50}, {E1_A1_C2_E2, 200}, {REST, 50}, {E1_A1_C2_E2, 200}, {REST, 50}, {E1_A1_C2_E2, 200}, {REST, 50}, {E1_A1_C2_E2, 200}, {REST, 50}, {E1_A1_C2_E2, 200}, {REST, 50},
+  { {E1_A1_C2_E2, 150}, {REST, 100}, {E1_A1_C2_E2, 150}, {REST, 100}, {E1_A1_C2_E2, 150}, {REST, 100}, {E1_A1_C2_E2, 150}, {REST, 100}, {E1_A1_C2_E2, 150}, {REST, 100}, {E1_A1_C2_E2, 150}, {REST, 100},
     {KE1, 125}, {KA1, 125}, {KC2, 125}, {KE2, 125},
-    {F1_A1_C2_DS2, 200}, {REST, 50}, {F1_A1_C2_DS2, 200}, {REST, 50}, {F1_A1_C2_DS2, 200}, {REST, 50}, {F1_A1_C2_DS2, 200}, {REST, 50}, {F1_A1_C2_DS2, 200}, {REST, 50}, {F1_A1_C2_DS2, 200}, {REST, 50},
+    {F1_A1_C2_DS2, 150}, {REST, 100}, {F1_A1_C2_DS2, 150}, {REST, 100}, {F1_A1_C2_DS2, 150}, {REST, 100}, {F1_A1_C2_DS2, 150}, {REST, 100}, {F1_A1_C2_DS2, 150}, {REST, 100}, {F1_A1_C2_DS2, 150}, {REST, 100},
     {KF1, 125}, {KA1, 125}, {KC2, 125}, {KDS2, 125}
   },
 
   // full loop (so much money)
-  { {E1_A1_C2_E2, 200}, {REST, 50}, {E1_A1_C2_E2, 200}, {REST, 50}, {E1_A1_C2_E2, 200}, {REST, 50}, {E1_A1_C2_E2, 200}, {REST, 50}, {E1_A1_C2_E2, 200}, {REST, 50}, {E1_A1_C2_E2, 200}, {REST, 50},
+  { {E1_A1_C2_E2, 150}, {REST, 100}, {E1_A1_C2_E2, 150}, {REST, 100}, {E1_A1_C2_E2, 150}, {REST, 100}, {E1_A1_C2_E2, 150}, {REST, 100}, {E1_A1_C2_E2, 150}, {REST, 100}, {E1_A1_C2_E2, 150}, {REST, 100},
     {KE1, 125}, {KA1, 125}, {KC2, 125}, {KE2, 125},
-    {F1_A1_C2_DS2, 200}, {REST, 50}, {F1_A1_C2_DS2, 200}, {REST, 50}, {F1_A1_C2_DS2, 200}, {REST, 50}, {F1_A1_C2_DS2, 200}, {REST, 50}, {F1_A1_C2_DS2, 200}, {REST, 50}, {F1_A1_C2_DS2, 200}, {REST, 50},
+    {F1_A1_C2_DS2, 150}, {REST, 100}, {F1_A1_C2_DS2, 150}, {REST, 100}, {F1_A1_C2_DS2, 150}, {REST, 100}, {F1_A1_C2_DS2, 150}, {REST, 100}, {F1_A1_C2_DS2, 150}, {REST, 100}, {F1_A1_C2_DS2, 150}, {REST, 100},
     {KF1, 125}, {KA1, 125}, {KC2, 125}, {KDS2, 125},
-    {F1_A1_D2, 450}, {REST, 50},
-    {D1_F1_C2, 200}, {REST, 50}, {D1_F1_A1, 200}, {REST, 50}, {D1_FS1_C2, 200}, {REST, 50}, {D1_FS1_C2, 500}, {C1_E1_A1, 1250},
+    {F1_A1_D2, 400}, {REST, 100},
+    {D1_F1_C2, 150}, {REST, 100}, {D1_F1_A1, 150}, {REST, 100}, {D1_GS1_C2, 150}, {REST, 100}, {D1_GS1_C2, 500}, {C1_E1_A1, 1250},
     {A1_C2_E2_A2, 250}, {REST, 250}, {REST, 500}
   }
 };
 
 void setup() {
-  /*
-    for (int i = 2; i <= 22; i++) {
+
+  for (int i = 2; i <= 13; i++) {
     pinMode(i, OUTPUT);
-    }
-    pinMode(24, OUTPUT);
-    pinMode(26, OUTPUT);
-    pinMode(28, OUTPUT);
-    pinMode(30, OUTPUT);*/
+  }
+  pinMode(32, OUTPUT);
+  pinMode(34, OUTPUT);
+  pinMode(36, OUTPUT);
+  pinMode(38, OUTPUT);
+  pinMode(40, OUTPUT);
+  pinMode(42, OUTPUT);
+  pinMode(44, OUTPUT);
+  pinMode(46, OUTPUT);
+
+  pinMode(24, OUTPUT);
+  pinMode(26, OUTPUT);
+  pinMode(28, OUTPUT);
+  pinMode(30, OUTPUT);
 
   Serial.begin(9600);
+
 }
 
 
 void loop() {
-  MOS_Call(MoneyListener);
-  //MOS_Call(PlayLoop);
-}
+  /* NON-SCHEDULER CODE*/
 
-void PlayLoop(PTCB tcb) {
-  MOS_Continue(tcb);
-  while (1) {
+  startMillis = millis(); // Start of sample window
+
+  // collect data for 50 mS
+  //while (millis() - startMillis < sampleWindow) {
+  sample = analogRead(0);
+
+  Serial.println(sample);
+
+  if (sample >= 678 && startMillis - lastTip >= 5000) {
+    lastTip = millis();
+
+    peaks++;
+    Serial.print("thanks! ");
+    Serial.println(peaks);
+
+    if (CurrLoop < 5) {
+      CurrLoop++;
+      LoopPosition = 0;
+    }
+  }
+
+  // no tip for 30 seconds
+  else if (CurrLoop > 0 && sample < 678 && startMillis - lastTip >= 30000) {
+    Serial.println("no tip? :(");
+    CurrLoop--;
+    lastTip = millis();
+    LoopPosition = 0;
+  }
+
+  //}
+
+  switch (CurrLoop) {
+    case 0:
+    case 1:
+      LoopLength = 2;
+      break;
+    case 2:
+      LoopLength = 16;
+      break;
+    case 3:
+      LoopLength = 26;
+      break;
+    case 4:
+      LoopLength = 32;
+      break;
+    case 5:
+      LoopLength = 45;
+      break;
+  }
+
+  if (!playingNote) {
+
     LoopNote = Loops[CurrLoop][LoopPosition % LoopLength][0];
     LoopDuration = Loops[CurrLoop][LoopPosition % LoopLength][1];
 
     // The track is 'muted' if the loop is playing a rest note
     if (LoopNote == REST) {
-      MOS_Delay(tcb, LoopDuration);
+      //delay(LoopDuration);
+      noteStartTime = millis(); 
     }
 
     else if (LoopNote == E1_A1_C2_E2) {
@@ -131,7 +201,7 @@ void PlayLoop(PTCB tcb) {
       digitalWrite(KC2, HIGH);
       digitalWrite(KE2, HIGH);
 
-      MOS_Delay(tcb, LoopDuration);
+      delay(LoopDuration);
 
       digitalWrite(KE1, LOW);
       digitalWrite(KA1, LOW);
@@ -145,7 +215,7 @@ void PlayLoop(PTCB tcb) {
       digitalWrite(KC2, HIGH);
       digitalWrite(KDS2, HIGH);
 
-      MOS_Delay(tcb, LoopDuration);
+      delay(LoopDuration);
 
       digitalWrite(KF1, LOW);
       digitalWrite(KA1, LOW);
@@ -158,7 +228,7 @@ void PlayLoop(PTCB tcb) {
       digitalWrite(KA1, HIGH);
       digitalWrite(KD2, HIGH);
 
-      MOS_Delay(tcb, LoopDuration);
+      delay(LoopDuration);
 
       digitalWrite(KF1, LOW);
       digitalWrite(KA1, LOW);
@@ -170,7 +240,7 @@ void PlayLoop(PTCB tcb) {
       digitalWrite(KF1, HIGH);
       digitalWrite(KC2, HIGH);
 
-      MOS_Delay(tcb, LoopDuration);
+      delay(LoopDuration);
 
       digitalWrite(KD1, LOW);
       digitalWrite(KF1, LOW);
@@ -182,19 +252,19 @@ void PlayLoop(PTCB tcb) {
       digitalWrite(KF1, HIGH);
       digitalWrite(KA1, HIGH);
 
-      MOS_Delay(tcb, LoopDuration);
+      delay(LoopDuration);
 
       digitalWrite(KD1, LOW);
       digitalWrite(KF1, LOW);
       digitalWrite(KA1, LOW);
     }
 
-    else if (LoopNote == D1_FS1_C2) {
+    else if (LoopNote == D1_GS1_C2) {
       digitalWrite(KD1, HIGH);
       digitalWrite(KFS1, HIGH);
       digitalWrite(KC2, HIGH);
 
-      MOS_Delay(tcb, LoopDuration);
+      delay(LoopDuration);
 
       digitalWrite(KD1, LOW);
       digitalWrite(KFS1, LOW);
@@ -206,7 +276,7 @@ void PlayLoop(PTCB tcb) {
       digitalWrite(KE1, HIGH);
       digitalWrite(KA1, HIGH);
 
-      MOS_Delay(tcb, LoopDuration);
+      delay(LoopDuration);
 
       digitalWrite(KC1, LOW);
       digitalWrite(KE1, LOW);
@@ -215,14 +285,14 @@ void PlayLoop(PTCB tcb) {
 
     else if (LoopNote == A1_C2_E2_A2) {
       digitalWrite(KA1, HIGH);
-      MOS_Delay(tcb, 20);
+      delay(20);
       digitalWrite(KC2, HIGH);
-      MOS_Delay(tcb, 20);
+      delay(20);
       digitalWrite(KE2, HIGH);
-      MOS_Delay(tcb, 20);
+      delay(20);
       digitalWrite(KA2, HIGH);
 
-      MOS_Delay(tcb, LoopDuration - 60);
+      delay(LoopDuration);
 
       digitalWrite(KA1, LOW);
       digitalWrite(KC2, LOW);
@@ -232,7 +302,7 @@ void PlayLoop(PTCB tcb) {
 
     else {
       digitalWrite(LoopNote, HIGH);
-      MOS_Delay(tcb, LoopDuration);
+      delay(LoopDuration);
       digitalWrite(LoopNote, LOW);
     }
 
@@ -241,62 +311,6 @@ void PlayLoop(PTCB tcb) {
 
 }
 
-// listens for money
-void MoneyListener(PTCB tcb) {
-  MOS_Continue(tcb);
-  while (1) {
-
-    startMillis = millis(); // Start of sample window
-
-    // collect data for 50 mS
-    while (millis() - startMillis < sampleWindow) {
-      sample = analogRead(0);
-
-      if (sample >= 678) {
-        lastTip = millis();
-
-        peaks++;
-        Serial.print("thanks! ");
-        Serial.println(peaks);
-
-        if (CurrLoop < 5) {
-          CurrLoop++;
-
-          // wait for 5 seconds before listening for the next tip
-          MOS_Delay(tcb, 5000);
-        }
-      }
-
-      // no tip for 30 seconds
-      else if (CurrLoop > 0 && sample < 678 && startMillis - lastTip >= 30000) {
-        Serial.println("no tip? :(");
-        CurrLoop--;
-        lastTip = millis();
-      }
-
-    }
-
-    switch (CurrLoop) {
-      case 0:
-      case 1:
-        LoopLength = 2;
-        break;
-      case 2:
-        LoopLength = 16;
-        break;
-      case 3:
-        LoopLength = 26;
-        break;
-      case 4:
-        LoopLength = 32;
-        break;
-      case 5:
-        LoopLength = 45;
-        break;
-    }
-  }
-
-}
 
 
 
